e83537671ba5279f27ef8496078c2c31432056e8
unknown <Robert.Aron.tx@gmail.com>
2021-03-07 16:59:05 -0600

    skeletons on the home page

--- www/src/views/Home.vue
+++ www/src/views/Home.vue
@@ -2,40 +2,46 @@
   <div class="media-items">
     <!-- TODO: card that goes to the browse thing -->
     <!-- https://mediasilo.com/ -->
-    <b-link :to="{ name: 'Upload'}" class='descrete-link'>
-      <b-card class="card" body-class='create-card' to="yo">
-        <h4>Upload New File <b-icon-upload/></h4>
-      </b-card>
-    </b-link>
-
-    <b-card v-for="item in content" :key="item.id" no-body class="card">
-      <b-button :href="item.downloadUrl" download class="floatingButton" variant="outline-primary">
-        <b-icon icon="download" aria-label="download" />
-      </b-button>
-      <b-link :to="{ name: 'FileDetails', params: { id: item.id } }">
-        <div class="picture-area">
-          <picture v-if="item.imageSrc">
-            <img :src="item.imageSrc" :alt="item.title" />
-          </picture>
-          <picture v-else>
-            <ClipIcon width="100%" height="100%" />
-          </picture>
-        </div>
-        <b-card-body>
-          <h4>{{ item.title }}</h4>
-          <p>
-            <small>
-              Uploaded {{ new Date(item.uploadedTimestamp).toLocaleString() }}
-            </small>
-          </p>
-        </b-card-body>
+    <div>
+      <b-link :to="{ name: 'Upload'}" class='descrete-link'>
+        <b-card class="card" body-class='create-card' to="yo">
+          <h4>Upload New File <b-icon-upload/></h4>
+        </b-card>
       </b-link>
-    </b-card>
+    </div>
+    <div v-for="item in content" :key="item.id">
+      <b-card no-body class="card">
+        <b-button :href="item.downloadUrl" download class="floatingButton" variant="outline-primary">
+          <b-icon icon="download" aria-label="download" />
+        </b-button>
+        <b-link :to="{ name: 'FileDetails', params: { id: item.id } }">
+          <div class="picture-area">
+            <picture v-if="item.imageSrc">
+              <img :src="item.imageSrc" :alt="item.title" />
+            </picture>
+            <picture v-else>
+              <ClipIcon width="100%" height="100%" />
+            </picture>
+          </div>
+          <b-card-body class="picture-body">
+            <h4>{{ item.title }}</h4>
+            <p>
+              <small>
+                Uploaded {{ new Date(item.uploadedTimestamp).toLocaleString() }}
+              </small>
+            </p>
+          </b-card-body>
+        </b-link>
+      </b-card>
+    </div>
+    <div v-for="n in isLoading?2:0" :key='n'>
+      <b-skeleton-img/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Content } from "@/fakeDB";
+import { Content } from "@/FileData";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import ClipIcon from "../assets/clip.svg";
 import { listFiles, tempRes } from "../sdk";
@@ -64,6 +70,18 @@ export default class HelloWorld extends Vue {
   display: grid;
   gap: var(--spacing);
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  > div {
+    padding-top: 100%;
+    position: relative;
+    > * {
+      position: absolute;
+      display: block;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
+  }
 }
 
 .picture-area {
@@ -85,8 +103,8 @@ export default class HelloWorld extends Vue {
 .floatingButton {
   background: white;
   position: absolute;
-  right: calc(var(--spacing) / 2);
-  top: calc(var(--spacing) / 2);
+  right:20px;
+  top: 20px;
 }
 .create-card{
   display: flex;
@@ -100,5 +118,18 @@ a {
   :hover {
     text-decoration: underline;
   }
+}
+
+.picture-body{
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  color: white;
+  background: linear-gradient(
+    to bottom,
+    rgba(0,0,0,.0) 0%,
+    rgba(0,0,0,.5) 30%,
+  )
 }
 </style>
